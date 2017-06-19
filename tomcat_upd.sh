ae88b819d71424a6d465a44ccfe09f7f0199d2df
asinkavets <Aliaksandr.Sinkavets@viacomcontractor.com>
2017-06-19 16:32:41 +0300

    refactoring deployment section, now it's fully working, except for Healthcheck and cleanupping old files

--- tomcat_upd.sh
+++ tomcat_upd.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 instance=""
-art_name="sample.war"
-#art_source="https://tomcat.apache.org/tomcat-8.0-doc/appdev/sample/sample.war"
-art_source="/$art_name"
+art_name="sample-v2.war"
+art_source=" /$art_name"
 art_store="/opt/bitnami/apache-tomcat/artifacts"
 art_dest="/opt/bitnami/apache-tomcat/webapps/ROOT"
+#art_source="https://tomcat.apache.org/tomcat-8.0-doc/appdev/sample/sample.war"
   #Bitnami info: https://docs.bitnami.com/aws/infrastructure/tomcat/
-  #1 stop Tomcatsample-v2.war
-#cmd1="sudo /opt/bitnami/ctlscript.sh stop"
-  #2 wget war file and put it in war's store, if art_store doesn't exist, create it
-#cmd2="sudo wget -O $art_dest/$art_name $art_source || (sudo mkdir $art_dest && sudo wget -O $art_dest/$art_name $art_source)"
-  #3 cleanuping site ROOT folder and extracting new artifact in it
-#cmd3="sudo rm -r $art_dest/* && sudo unzip $art_store/$art_name -d $art_dest"
-  #4 start Tomcat
-#cmd4="sudo /opt/bitnami/ctlscript.sh start"
-  #5 healthcheck
-#cmd5="curl -o /dev/null --silent --head --write-out '%{http_code}\n' localhost"
-  # while cmd5 no 200, sleep 3
-  # clean old war's
-#ssh -o StrictHostKeyChecking=no -l bitnami $instance "date"
-ssh -o StrictHostKeyChecking=no -l bitnami $instance 'bash -s' <<'ENDSSH'
+
+ssh -o StrictHostKeyChecking=no -l bitnami -t $instance "
   # stop Tomcat Server
-echo "[STEP]: Stopping Tomcat Server"
+echo ''
+echo '[STEP]: Stopping Tomcat Server'
 sudo /opt/bitnami/ctlscript.sh stop
   # wget war file and put it in war's store, if art_store doesn't exist, create it
-echo "[STEP]: Downloading artifact and extracting it"
-sleep 5
-sudo wget -O $art_dest/$art_name $art_source || (sudo mkdir $art_dest && sudo wget -O $art_dest/$art_name $art_source)
+echo ''
+echo '[STEP]: Downloading artifact'
+echo ''
+sudo wget -O $art_store/$art_name $art_source || (sudo mkdir $art_store && sudo wget -O $art_store/$art_name $art_source)
   # cleanuping site ROOT folder and extracting new artifact in it
-sleep 5e
-sudo rm -r $art_dest/* && sudo unzip $art_store/$art_name -d $art_dest
-  # start Tomcat
-echo "[STEP]: Starting Tomcat Server"
-sleep 5
+echo ''
+echo '[STEP]: Extracting new artifact'
+echo ''
+(sudo rm -r $art_dest/* && sudo unzip $art_store/$art_name -d $art_dest) || (sudo unzip -o $art_store/$art_name -d $art_dest)
+    # start Tomcat
+echo ''
+echo '[STEP]: Starting Tomcat Server'
+echo ''
 sudo /opt/bitnami/ctlscript.sh start
   # HealthCheck
-echo "[STEP]: Checking if application is ready for work"
-until [[ "$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' localhost)" == 200 ]]; do
-  echo "->  waiting for Web Server"
-  echo "    ....trying again......"
-  sleep 3
-done
-echo "[STEP]: New application version has been successfully deployed to: $instance"
-  echo "Successfull"
-ENDSSH
+#echo ''
+#echo '[STEP]: Checking if application is ready for work'
+#echo ''
+#until [[ "$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' localhost)" == 200 ]]; do
+#  echo '->  waiting for Web Server'
+#  echo '    ....trying again......'
+#  sleep 5
+#done
+echo ''
+echo '[STEP]: New application version has been successfully deployed to: $instance'
+echo '[Successfull]'
+"
+#PENDING
+#[STEP] healthcheck
+#cmd5="curl -o /dev/null --silent --head --write-out '%{http_code}\n' localhost"
+#[STEP] clean old war files
