b77990aec532e44e07e85cd84d8b40f4dbc350f7
Aliaksandr Sinkavets <Aliaksandr.Sinkavets@viacomcontractor.com>
2017-06-23 12:35:25 +0300

    - adding params-example.txt file - adding tomcat_upd.sh functionality to the main script deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ elif ! [[ -f "$1" ]]; then
 fi
 
   #1 define AWS profile and exporting to ENV
-AWS_PROFILE_NAME=saml
+AWS_PROFILE_NAME=default
 export AWS_PROFILE=$AWS_PROFILE_NAME
 
   #2 getting variables from parameters file and brake the script if file does not exists
@@ -33,9 +33,55 @@ done
   #4 'for' loop to process within each of EC2 instances in ELB
 for ec2 in "${ec2_list[@]}"; do
   #4.1 Printing instance ID
-  echo "Starting work with instance: $ec2"
+  echo "Starting work on instance: $ec2"
   #4.2 Checking EC2 instance initial status in ELB service and printing it out
   ec2_status="$(aws elb describe-instance-health --load-balancer-name $elb_name --instances $ec2 --query 'InstanceStates[*].[State]' --output text)"
-  echo "  -> initial item $ec2 ELB status: $ec2_status";
+  echo "  -> initial item $ec2 ELB status: $ec2_status"
+  #4.3 Disconnect EC2 instance out from ELB rotation
+  echo "  -> disconnecting $ec2 from ELB rotation ..."
+  aws elb deregister-instances-from-load-balancer --load-balancer-name $elb_name --instances $ec2
+  #4.4 Convert EC2 ID to IP/CNAME
+  ec2_ip="$(aws ec2 describe-instances --instance-ids $ec2 --query 'Reservations[*].Instances[*].PublicDnsName' --output text)"
+  #4.5 Connecting by ssh and deploying application
+  ssh -o StrictHostKeyChecking=no -l $ec2_username -t $ec2_ip "
+      # stop Tomcat Server
+    echo ''
+    echo '[STEP]: Stopping Tomcat Server'
+    sudo /opt/bitnami/ctlscript.sh stop
+      # wget war file and put it in war's store, if art_store doesn't exist, create it
+    echo ''
+    echo '[STEP]: Downloading artifact'
+    echo ''
+    sudo wget -O $art_store/$art_name $art_source || (sudo mkdir $art_store && sudo wget -O $art_store/$art_name $art_source)
+      # cleanuping site ROOT folder and extracting new artifact in it
+    echo ''
+    echo '[STEP]: Extracting new artifact'
+    echo ''
+    (sudo rm -r $art_dest/* && sudo unzip $art_store/$art_name -d $art_dest) || (sudo unzip -o $art_store/$art_name -d $art_dest)
+        # start Tomcat
+    echo ''
+    echo '[STEP]: Starting Tomcat Server'
+    echo ''
+    sudo /opt/bitnami/ctlscript.sh start
+  "
+  #4.6 Check instance status (health check)
+  echo "  -> checking $ec2 application health"
+  until [[ "$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $ec2_ip)" == 200 ]]; do
+    echo "  ->  waiting for Web Server"
+    echo "    ....trying again......"
+    sleep 5
+  done
+  echo "  -> Web Server health is OK"
+  #4.7 Getting instance back to ELB if HealthCheck successfull
+  echo "  -> get back $ec2 into ELB rotation"
+  aws elb register-instances-with-load-balancer --load-balancer-name $elb_name --instances $ec2
+  #4.8 Checking EC2 instance status in ELB service after artifact deployment
+  until [[ "$(aws elb describe-instance-health --load-balancer-name $elb_name --instances $ec2 --query 'InstanceStates[*].[State]' --output text)" == InService ]]; do
+    echo "  ->  waiting for instance $ec2 health in ELB"
+    echo "    ....trying again......"
+    sleep 3
+  done
+  echo ""
+  echo "  -> !Application has been successfully deployed to $ec2!"
 done
 exit 0
